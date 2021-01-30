eefd8f5eb60942f952ee1d95b9368db9c8df39c7
Debanjan2001 <debanjansaha2001@gmail.com>
2021-01-31 03:23:28 +0530

    Okayish

---
+++ Assignment 2/Main.cpp
@@ -0,0 +1,14 @@
+/************ C++ Headers ************************************/
+#include <iostream>
+using namespace std;
+
+/************ PROJECT Headers ********************************/
+#include "Fraction.hpp"
+
+void TestFraction();
+
+int main(){
+    TestFraction();
+    
+    return 0;
+}
