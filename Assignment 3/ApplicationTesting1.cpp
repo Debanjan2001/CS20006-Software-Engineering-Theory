b683e3568f57299593ac6023bc060fff216db5f8
Debanjan Saha <debanjansaha2001@gmail.com>
2021-03-07 15:48:53 +0530

    Fixes

---
+++ Assignment 3/ApplicationTesting1.cpp
@@ -0,0 +1,42 @@
+/*---------------- 
+Debanjan Saha
+19CS30014
+-----------------*/
+
+// ***** C++ Standard Library Headers
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// ***** Project Headers
+#include "Date.h"
+#include "Station.h"
+#include "BookingClasses.h"
+#include "Booking.h"
+
+
+// Test application 1 for booking.
+void BookingApplicationTest1()
+{
+    Booking b1(Station("Kolkata"), Station("Chennai"), Date(14,7,2021), ACFirstClass::Type());
+    Booking b2(Station("Chennai"), Station("Bangalore"), Date(15,7,2021), AC2Tier::Type());
+    Booking b3(Station("Bangalore"), Station("Delhi"), Date(16,7,2021), FirstClass::Type());
+    Booking b4(Station("Delhi"), Station("Mumbai"), Date(17,7,2021), ACChairCar::Type());
+    Booking b5(Station("Mumbai"), Station("Kolkata"), Date(18,7,2021), AC3Tier::Type());
+   
+    // Output the bookings done where sBookings is the collection of bookings done
+    vector<Booking*>::iterator it;
+    for (it = Booking::sBookings.begin(); it < Booking::sBookings.end(); ++it) {
+        cout << *(*it)<<endl;
+    }
+}
+
+
+
+int main()
+{
+    cout<<"// OUTPUT FOR TEST-1------------------------------"<<endl;
+    BookingApplicationTest1();
+    cout<<endl;
+}
--- Assignment 3/ApplicationTesting2.cpp
+++ Assignment 3/ApplicationTesting2.cpp
@@ -15,24 +15,7 @@ using namespace std;
 #include "BookingClasses.h"
 #include "Booking.h"
 
-
-// Test application 1 for booking.
-void BookingApplicationTest1()
-{
-    Booking b1(Station("Kolkata"), Station("Chennai"), Date(14,7,2021), ACFirstClass::Type());
-    Booking b2(Station("Chennai"), Station("Bangalore"), Date(15,7,2021), AC2Tier::Type());
-    Booking b3(Station("Bangalore"), Station("Delhi"), Date(16,7,2021), FirstClass::Type());
-    Booking b4(Station("Delhi"), Station("Mumbai"), Date(17,7,2021), ACChairCar::Type());
-    Booking b5(Station("Mumbai"), Station("Kolkata"), Date(18,7,2021), AC3Tier::Type());
-   
-    // Output the bookings done where sBookings is the collection of bookings done
-    vector<Booking*>::iterator it;
-    for (it = Booking::sBookings.begin(); it < Booking::sBookings.end(); ++it) {
-        cout << *(*it)<<endl;
-    }
-}
-
-// Test application 2 for booking.
+// Test application 2 for booking from (Slides)
 void BookingApplicationTest2()
 {
     Booking b1(Station("Mumbai"), Station("Delhi"), Date(15, 2, 2021), ACFirstClass::Type());
@@ -53,11 +36,7 @@ void BookingApplicationTest2()
 
 int main()
 {
-    cout<<"OUTPUT FOR TEST-1"<<endl;
-    BookingApplicationTest1();
-    cout<<endl;
-
-    cout<<"OUTPUT FOR TEST-2 (from Slides)"<<endl;
+    cout<<"// OUTPUT FOR TEST-2------------------------------"<<endl;
     BookingApplicationTest2();
     cout<<endl;
 }
